83410f8e9419fa6d0488ec0e85ab6ff4b7a927ea
Youngmin Park <43109589+ympark99@users.noreply.github.com>
2021-01-18 19:42:49 +0900

    sort

---
+++ 1427.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    
+    vector<int> v;
+
+    while(n!=0){
+        v.push_back(n%10);
+        n /= 10;
+    }
+
+    sort(v.begin(),v.end(),greater<>());
+
+    for(int i =0; i<v.size();i++)
+        cout << v[i];
+
+    return 0;
+}
