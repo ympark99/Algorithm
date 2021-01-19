5d649ab4cad8cf26489e48617a2b5c11db05ba83
Youngmin Park <43109589+ympark99@users.noreply.github.com>
2021-01-19 19:06:41 +0900

    pair vector

---
+++ 11651.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    
+    vector<pair<int,int>> xy;
+
+    for(int i = 0 ; i<n; i++){
+        int num[2];
+        for(int j=0; j<2; j++){
+            cin >> num[j];
+        }
+        xy.push_back(make_pair(num[1],num[0]));
+    }
+
+    sort(xy.begin(),xy.end());
+
+    for(int i =0; i<xy.size();i++)
+        cout << xy[i].second << " " << xy[i].first << "\n";
+
+    return 0;
+}
