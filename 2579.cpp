9fb0d53eed96eebc7aafbbfabcd842d3e5568ca7
Youngmin Park <43109589+ympark99@users.noreply.github.com>
2021-01-27 19:48:11 +0900

    dp 5

---
+++ 2579.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int arr[300];
+    int dp[300];
+
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    dp[0] = arr[0];
+    dp[1] = max(arr[0]+arr[1], arr[1]);
+    dp[2] = max(arr[0]+arr[2], arr[1]+arr[2]);
+
+    for (int i = 3; i < n; i++) dp[i] = max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]);
+    cout << dp[n-1];
+
+    return 0;
+}
