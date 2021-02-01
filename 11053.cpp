f12e290a5143c727208b7eec4133537dbb894a96
Youngmin Park <43109589+ympark99@users.noreply.github.com>
2021-02-01 20:16:02 +0900

    Update 11053.cpp

--- 11053.cpp
+++ 11053.cpp
@@ -3,31 +3,32 @@
 
 using namespace std;
 
+int dp[1001];
+int arr[1001];
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     int n;
     cin >> n;
-
-    int* arr = new int[n+1];
-    int* dp = new int[n+1];
-    int cnt = 1;
-
-    for(int i=1; i<=n; i++){
+    for (int i=0; i< n; i++) {
         cin >> arr[i];
+        dp[i] = 1; //길이가 1인 경우로 초기화
     }
-
-    dp[0] = arr[0] = 0;
-    dp[1] = 1;
-
-    for(int i=2; i<=n; i++){
-        if(arr[i] <= arr[cnt]) dp[i] = dp[cnt];
-        else{
-            dp[i] = dp[cnt] + 1;
-            cnt = i;
+    for (int i=0; i < n; i++) {
+        int& last_val = arr[i];
+        for (int j=0; j < i; j++) {
+            int& target_val = arr[j];
+            if (last_val > target_val) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
         }
     }
-
-    cout << dp[n];
+    
+    int ans = 0;
+    for (int i=0; i<n; i++) {
+        ans = max(ans, dp[i]);
+    }
+    cout << ans << '\n';
+	return 0;
 }
