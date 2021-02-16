bb700453a508ec35d9b452ddb09012c2d3e7342a
Youngmin Park <43109589+ympark99@users.noreply.github.com>
2021-02-16 20:08:30 +0900

    solving

--- failure.cpp
+++ failure.cpp
@@ -4,38 +4,36 @@
 
 using namespace std;
 
-bool desc(int a, int b){
-    return a > b;
+bool desc(pair<double,double> a, pair<double,double> b){
+    return a.second > b.second;
 }
 
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
-    vector<int> stop(N+1);
-    vector<int> clear(N+1);
-    vector<int> fail(N+1);
-    vector<int> fail_sort(N+1);
+    vector<int> stop;
+    vector<int> chal;
+
+    for(int i = 0; i< N+1; i++){
+        stop.push_back(0);
+        chal.push_back(0);
+    }
+    vector<pair<double,double>> fail(N+1);
 
     for(int i=0; i<stages.size(); i++){
         stop[stages[i]] += 1;
     }
     
     for(int i=1; i<=N; i++){
-        clear[i] = clear[i-1] + stop[i];
-        fail[i] = stop[i] / clear[i];
-        fail_sort[i] = stop[i] / clear[i];
+        chal[i] = chal[i-1] + stop[i];
+        fail[i].first = (double)stop[i] / (double)chal[i];
+        fail[i].second = (double)stop[i] / (double)chal[i];
     }
 
-    stable_sort(fail_sort.begin(), fail_sort.end(),desc);
+    stable_sort(fail.begin(),fail.end(),desc);
 
-    for(int i=0; i<fail_sort.size(); i++){
-        for(int j=0; j<fail.size(); j++){
-            if(fail[j] == fail_sort[i]){
-                answer.push_back(j);
-                break;
-            }
-        }
+    for(int i=0; i< N; i++){
+        answer.push_back(fail[i].first);
     }
-
 
     return answer;
 }
