5f0fcc57498953e3009ca0b6aa1cd432532c4547
Youngmin Park <ympark1230@naver.com>
2021-06-03 11:40:06 +0900

    프로그래머스 카카오 실패율

--- failure.cpp
+++ failure.cpp
@@ -4,41 +4,36 @@
 
 using namespace std;
 
-bool desc(pair<int,double> a, pair<int,double> b){
+bool comp(pair<int,double> a, pair<int,double> b){
     if(a.second == b.second){
         return a.first < b.first;
     }
-    else return a.second > b.second;
+    return a.second > b.second;
 }
 
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
     vector<int> stop;
-    vector<int> chal;
+    vector<pair<int,double>> fail;
+    int chal = stages.size();
 
-    for(int i = 0; i< N+1; i++){
+    for(int i = 0; i < N + 1; i++){
         stop.push_back(0);
-        chal.push_back(0);
     }
-    vector<pair<int,double>> fail(N+1);
-
-    for(int i=0; i<stages.size(); i++){
-        stop[stages[i]] += 1;
+    for(int i = 0; i < stages.size(); i++){
+        stop[stages[i]]++;
     }
-    
-    for(int i=1; i<=N; i++){
-        chal[i] = chal[i-1] + stop[i];
-
-        fail[i].first = i;
-        if(chal[i] == 0) fail[i].second = 0;
-        else fail[i].second = (double)stop[i] / (double)chal[i]; // 실패율
+    for(int i = 1; i <= N; i++){
+        double failure = (double)stop[i] / (double)chal;
+        if(chal == 0) fail.push_back(make_pair(i,0));
+        else fail.push_back(make_pair(i, failure));
+        chal -= stop[i];
     }
 
-    sort(fail.begin(),fail.end(),desc);
+    sort(fail.begin(),fail.end(),comp);
 
-    for(int i=1; i<=N; i++){
+    for(int i=0; i<N; i++){
         answer.push_back(fail[i].first);
     }
-
     return answer;
 }
