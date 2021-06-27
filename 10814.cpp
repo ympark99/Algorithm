826b49916a74de1e9fcca880fe43d4c6e51f5807
Youngmin Park <ympark1230@naver.com>
2021-06-27 19:26:22 +0900

    백준 정렬 stable_sort

--- 10814.cpp
+++ 10814.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <string>
-#include <utility>
+#include <algorithm>
 
 using namespace std;
 
-bool compare(pair<int,string>v1, pair<int,string>v2){
-    return v1.first < v2.first;
+bool comp(pair<int,string> a, pair<int,string> b){
+   return a.first < b.first;
 }
 
-int main(void) {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-
-    int n;
-    cin >> n;
-    
-    vector<pair<int,string>> v(n);
-    
-
-    for(int i = 0 ; i<n; i++){
-        cin >> v[i].first >> v[i].second;
-    }
-
-    stable_sort(v.begin(),v.end(),compare);
-
-
-    for(int i = 0; i<v.size(); i++){
-        cout << v[i].first << " " << v[i].second << "\n";
-    }
-
-    return 0;
+int main(void){
+   ios_base::sync_with_stdio(0); cin.tie(0);
+   int N;
+   cin >> N;
+   vector<pair<int,string>> v(N);
+   for(int i = 0; i < N; i++){
+      cin >> v[i].first >> v[i].second;
+   }
+   stable_sort(v.begin(), v.end(), comp);
+   for(auto elem : v) cout << elem.first << " " << elem.second << '\n';
 }
