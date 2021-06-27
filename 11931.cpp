436829f882d859d802bdc5f6659b50b11a17428f
Youngmin Park <ympark1230@naver.com>
2021-06-27 19:13:02 +0900

    백준 정렬

---
+++ 11931.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+   ios_base::sync_with_stdio(0); cin.tie(0);
+   int N;
+   cin >> N;
+   vector<int> v(N);
+   for(int i = 0; i < N; i++){
+      cin >> v[i];
+   }
+   sort(v.begin(), v.end(), greater<int>());
+   for(auto elem : v) cout << elem << '\n';
+}
