84ebd7536f1c3f1896a4b15f4da20b9e07c7575b
Youngmin Park <ympark1230@naver.com>
2021-06-27 17:55:28 +0900

    백준 stack

--- 9012.cpp
+++ 9012.cpp
@@ -1,33 +1,32 @@
 #include <iostream>
-#include <stack>
 #include <string>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
 
 int main(void){
-	int n;
-	cin >> n;
-	for(int i=0; i<n; i++){
-		string str;
-		cin >> str;
-		stack<char> st;
-		char ch;
-		bool isNo = false;
+   ios_base::sync_with_stdio(0); cin.tie(0);
+   int T;
+   cin >> T;
 
-		for(int j=0; j<str.size(); j++){
-			if(str[j] == ')'){
-				if(st.empty()){
-					isNo = true;
-					break;
-				}
-				else st.pop();
-			}else st.push(str[j]);
-		}
-		if(isNo) cout << "NO" << endl;
-		else if(!st.empty()) cout << "NO" << endl;
-		else cout << "YES" << endl;
-	}
-
-
-	return 0;
+   for(int i = 0; i < T; i++){
+      string str;
+      stack<char> st;
+      cin >> str;
+      bool isNO = false;
+      for(int j = 0; j < str.size(); j++){
+         if(str[j] == ')'){ 
+            if(st.empty()){
+               isNO = true;
+               break;
+            }
+            else st.pop();
+         }
+         else st.push('(');
+      }
+      if(isNO == true) cout << "NO" << '\n';
+      else if(!st.empty()) cout << "NO" << '\n';
+      else cout << "YES" << '\n';
+   }
 }
