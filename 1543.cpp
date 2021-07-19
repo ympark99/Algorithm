95435d744031c911d3f033844c9893b6f48c1bb4
Youngmin Park <ympark1230@naver.com>
2021-07-19 20:43:25 +0900

    백준 문자열

--- 1543.cpp
+++ 1543.cpp
@@ -1,37 +1,35 @@
-#include <iostream>
-#include <string>
-
+#include<iostream>
+#include<string>
 using namespace std;
-
-int main(void){
-   ios_base::sync_with_stdio(0); cin.tie(0);
-
-   string str, search;
-
-   getline(cin, str);
-   getline(cin, search);
-   
-   if(search.length() > str.length()){
-      cout << 0;
-      return 0;
-   }
-
-   int answer = 0;
-
-   for(int i = 0; i < str.length() - search.length();){
-      bool check = true;
-      for(int j = 0; j < search.length(); j++){
-         if(str[i + j] != search[j]){
-            check = false;
-            break;
-         }
-      }
-      if(check){
-         answer++;
-         i += search.length();
-      }
-      else i++;
-   }
-   
-   cout << answer;
+int main()
+{
+	string s1, s2;
+	getline(cin, s1);
+	getline(cin, s2);
+	int cnt = 0;
+	if (s1.size() < s2.size())
+	{
+		cout << 0 << endl;
+		return 0;
+	}
+	for (int i = 0; i <= s1.size()-s2.size();)
+	{
+		bool check = true;
+		for (int j = 0; j < s2.size(); j++)
+		{
+			if (s1[i + j] != s2[j])
+			{
+				check = false;
+				break;
+			}
+		}
+		if (check)
+		{
+			cnt++;
+			i += s2.size();
+		}
+		else
+			i++;
+	}
+	cout << cnt << endl;
 }
