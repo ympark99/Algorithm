8867a01b36b61292d249ce1f48887ef5440656ee
Youngmin Park <ympark1230@naver.com>
2021-08-03 19:05:19 +0900

    DP 2회차

--- 2579.cpp
+++ 2579.cpp
@@ -1,4 +1,35 @@
 #include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int dp[300 + 1] = {0, };
+    int score[300 + 1] = {0,};
+    
+    int n;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        cin >> score[i + 1];
+    }
+
+    dp[1] = score[1], dp[2] = dp[1] + score[2], dp[3] = max(score[1], score[2]) + score[3];
+
+    for(int i = 4; i <= n; i++){
+        dp[i] = max(dp[i-2] + score[i], dp[i-3] + score[i-1] + score[i]);
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
+
+
+/*
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
@@ -24,3 +55,4 @@ int main(){
 
     return 0;
 }
+*/
