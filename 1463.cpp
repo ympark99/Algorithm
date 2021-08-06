89c2182c6367b19ca35676e9417731b6a75fe880
Youngmin Park <ympark1230@naver.com>
2021-08-06 13:53:39 +0900

    dp

--- 1463.cpp
+++ 1463.cpp
@@ -4,23 +4,24 @@
 using namespace std;
 
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    const int MAXN = 1000000 + 1;
+
+    int dp[MAXN] =  {0, };
 
     int n;
-    int res;
     cin >> n;
+    
+    dp[1] = 0; 
 
-    int* arr = new int[n+1];
-    int* dp = new int[n+1];
-
-    dp[1] = 0;
-
-    for(int i=2; i<=n; i++){
-        if(i % 3 == 0)  dp[i] = min(dp[i/3],dp[i-1]) + 1;
-        else if(i % 2 ==0) dp[i] = min(dp[i/2],dp[i-1]) + 1;
-        else dp[i] = dp[i-1] + 1;     
+    for(int i = 2; i <= n; i++){
+        dp[i] = dp[i - 1] + 1;
+        if(i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+        if(i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
     }
 
     cout << dp[n];
+
     return 0;
 }
