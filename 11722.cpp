d1b0b545cf1854e729243f828b6b368b06ca5a66
Youngmin Park <ympark1230@naver.com>
2021-08-09 20:50:19 +0900

    DP LIS 감소

---
+++ 11722.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[1000 + 1];
+int dp[1000 + 1];
+
+int main(void)
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];    
+    
+    int res = 0;
+    for(int i = 0; i < n; i++){
+        dp[i] = 1;
+        for(int j = 0; j < i; j++){
+            if(arr[j] > arr[i])
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+        res = max(res, dp[i]);
+    }
+
+    cout << res;
+
+    return 0;
+}
