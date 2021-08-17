faa73477ef1d21c40ac8c0e4bf807945e8585fe2
Youngmin Park <ympark1230@naver.com>
2021-08-17 11:31:46 +0900

    DP 가장 긴 부분수열 NlogN

---
+++ 2352.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int n, y;
+    cin >> n;
+
+    vector<int> v;
+
+    for(int i = 0; i < n; i++){
+        cin >> y;
+        if(v.empty() || v.back() < y) v.push_back(y);
+        else{
+            auto iter =  lower_bound(v.begin(), v.end(), y);
+            *iter = y;
+        }
+    }
+
+    cout << (int)(v.size());
+    return 0;
+}
