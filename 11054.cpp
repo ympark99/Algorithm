75e62a5d3f02a64814ab512db31d1e25a203e59b
Youngmin Park <ympark1230@naver.com>
2021-08-18 15:19:14 +0900

    DP LIS

--- 11054.cpp
+++ 11054.cpp
@@ -3,39 +3,40 @@
 
 using namespace std;
 
-int arr[1000 + 1];
-int dp_up[1000 + 1] = {0, };
-int dp_down[1000 + 1] = {0, };
-
 int main(void)
 {
     ios::sync_with_stdio(false); cin.tie(NULL);
 
-    int n;
-    cin >> n;
-
-    for(int i = 0; i < n; i++){
+    int N;
+    int arr[1001];
+    int dp[2][1001];
+    
+    cin >> N;
+    for(int i = 1; i <= N; i++){
         cin >> arr[i];
     }
 
-    int res_up, res_down = 0;
-    int idx_up, idx_down = 0;
-
-    for(int i = 0; i < n; i++){
-        dp_up[i] = 1;
-        dp_down[i] = 1;
-        for(int j = 0; j < i; j++){
-            if(arr[i] > arr[j])
-                dp_up[i] = max(dp_up[i], dp_up[j] + 1);
-            else if(arr[i] < arr[j])
-                dp_down[i] = max(dp_down[i], dp_down[j] + 1);                
-        }
-
-        res_up = max(res_up, dp_up[i]);
-        res_down = max(res_down, dp_down[i]);
+    for(int i = 1; i <= N; i++){
+        dp[0][i] = 1;
+        for(int j = 1; j < i; j++)
+            if(arr[i] > arr[j] && dp[0][i] < dp[0][j] + 1)
+                dp[0][i] = dp[0][j] + 1;
     }
-
-    cout << res_up;
-
+    
+    for(int i = N; i >= 1; i--){
+        dp[1][i] = 1;
+        for(int j = N; j > i; j--)
+            if(arr[i] > arr[j] && dp[1][i] < dp[1][j] + 1)
+                dp[1][i] = dp[1][j] + 1;
+    }
+    
+    int max = 0;
+    for(int i = 1; i <= N; i++){
+        if(max < dp[0][i] + dp[1][i] - 1)
+            max = dp[0][i] + dp[1][i] - 1;
+    }
+    
+    cout << max << '\n';
+    
     return 0;
 }
