0b853c136a616982463beb76c1bd5b88482fd287
Youngmin Park <ympark1230@naver.com>
2021-09-01 11:51:42 +0900

    백준 DP 강의실 배정(추가 solve 필요)

---
+++ 11000.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+bool comp(pair<int, int> &a, pair<int, int> &b){
+    if(a.first == b.first) return a.second < b.second;
+    return a.first < b.first;
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    deque<pair<int, int>> dq;
+
+    int n;
+    cin >> n;
+
+    vector<int> v;
+
+    while(n--){
+        int s, t;
+        cin >> s >> t;
+        dq.push_back(make_pair(s, t));
+    }
+    
+    sort(dq.begin(), dq.end(), comp);
+
+    int answer = 0;
+    int cnt = 0;
+    while(!dq.empty()){
+        int i = 0;
+        while(i < dq.size()){
+            if(v.empty()){
+                v.push_back(dq.front().second);
+                answer++;
+                dq.pop_front();
+                continue;
+            }
+            if(dq[i].first >= v.back()){
+                v.push_back(dq[i].second);
+                dq.pop_front();
+            }
+            else i++;           
+        }
+        v.clear();
+    }
+    cout << answer;
+}
