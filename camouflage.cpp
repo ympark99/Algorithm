3d933a5453d36e1abfd8cb7a83b0c6e5cdf3f07d
Youngmin Park <ympark1230@naver.com>
2021-12-04 15:35:32 +0900

    Update camouflage.cpp

--- camouflage.cpp
+++ camouflage.cpp
@@ -7,12 +7,13 @@ using namespace std;
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
     map<string, int> m;
-    for(auto cloth : clothes){
-        m[cloth[1]] += 1;
-    }
     map<string, int>::iterator iter;
+
+    for(int i = 0; i < clothes.size(); i++){
+        m[clothes[i][1]]++;
+    }    
     for(iter = m.begin(); iter != m.end(); iter++){
-        answer *= iter -> second + 1;
+        answer *= (iter->second + 1);
     }
     return answer - 1;
 }
