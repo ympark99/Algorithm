487d1dd650a25ff8c0677d73bff645a603bebaea
Youngmin Park <ympark1230@naver.com>
2022-01-06 00:09:53 +0900

    백준 팰린드롬 만들기 문자열

--- 1213.cpp
+++ 1213.cpp
@@ -51,3 +51,60 @@ int main(){
 
 	return 0;
 } 
+
+/*
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int alpha[26];
+int midIdx = -1;
+
+bool isNotPalin(){
+    int cnt = 0;
+    for (int i = 0; i < 26; i++){
+        if (alpha[i] > 0){
+            if (alpha[i] % 2 == 1){
+                midIdx = i;
+                cnt++;
+            }
+        }
+    }
+    return cnt >= 2;
+}
+
+int main(){
+	ios::sync_with_stdio(false); cin.tie(NULL);
+
+    string str; cin >> str;
+
+    for (int i = 0; i < str.length(); i++)
+        alpha[str[i] - 'A']++;
+
+    if (isNotPalin()){
+        printf("I'm Sorry Hansoo");
+        //cout << "I'm sorry Hansoo";
+        return 0;
+    }
+    else {
+        string answer = "";
+        string back = "";
+        for (int i = 0; i < 26; i++) {
+            if (alpha[i] > 0) {
+                for (int j = 0; j < alpha[i] / 2; j++) {
+                    answer += i + 'A';
+                }
+            }
+        }
+        back = answer;
+        reverse(back.begin(), back.end());
+        if (midIdx != -1) {
+            answer += midIdx + 'A';
+        }
+        answer += back;
+        cout << answer;
+    }
+}
+*/
