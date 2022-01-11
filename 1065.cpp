c7caf769af7d17fd8a3358bcb7bc186a398db433
Youngmin Park <ympark1230@naver.com>
2022-01-11 15:07:39 +0900

    백준 한수 브루트포스

---
+++ 1065.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false); cin.tie(NULL);
+
+	int N; cin >> N;
+
+	if(N < 100) cout << N;
+	else if(N == 1000) cout << 144;
+	else{
+		int cnt = 99;
+		int a, b, c = 0;
+		for(int i = 100; i <= N; i++){
+			a = i % 10;
+			b = (i / 10) % 10;
+			c = (i / 100) % 10;
+
+			if(a < b && (b - a) == (c - b)) cnt++;
+			else if(b < a && (a - b) == (b - c)) cnt++;
+			else if(a == b && b == c) cnt++;
+		}
+
+		cout << cnt;
+	}
+
+	return 0;
+}
