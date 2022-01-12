cbdb4bfaf79961deea665672d4c01aa45907ff01
Youngmin Park <ympark1230@naver.com>
2022-01-12 14:31:01 +0900

    백준 로또 브루트포스 dfs

---
+++ 6603.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false); cin.tie(NULL);
+
+	while(1){
+		/*
+		string str;
+		vector<string> v;
+		while(getline(cin, str, ' ')){
+			v.push_back(str);
+		}
+		
+		// if(v[0] == "0") break;
+		if(str.length() == 1 && str[0] == '0') break;	
+
+		int k = str[0] - '0';
+
+		// 공백 제거
+		// str.erase(remove(str.begin(), str.end(), ' '), str.end());			
+
+		// for(int i = 1; i < str.length(); i++)
+		// 	v.push_back(str[i]);
+
+		*/
+
+		int k; cin >> k;
+		if(k == 0) break;
+
+		vector<int> v(k);
+		for(int i = 0; i < k; i++)
+			cin >> v[i];
+
+		vector<bool> lotto(k, false);
+
+		// nCr 구현
+		for(int i = 0; i < 6; i++)
+			lotto[i] = true;		
+		
+		do{
+			for(int i = 0; i < v.size(); i++){
+				if(lotto[i]) cout << v[i] << ' ';
+			}
+			cout << '\n';
+		}while(prev_permutation(lotto.begin(), lotto.end()));
+			
+		v.clear();
+		lotto.clear();
+	}
+
+	return 0;
+}
