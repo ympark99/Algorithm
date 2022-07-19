37a21d93dbd8dbaccc26c7ee38c878f7363a49b4
Youngmin Park <ympark1230@naver.com>
2022-07-19 10:34:19 -0700

    백준 치킨 배달 백트래킹

--- 15686.cpp
+++ 15686.cpp
@@ -1,35 +1,63 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
+#include <queue>
+#include <algorithm>//min
+#include <cstring>//memset
+#include <cmath>//abs
 using namespace std;
 
-int arr[51][51];
+struct Pos {
+	int x, y;
+};
 
-void getComb(vector<pair<int, int>> store){
-    do{
-        /* code */
-    }while (next_permutation(store.begin(), store.end()));
-    
+int N, M;
+bool check[13];
+
+vector<Pos> House;
+vector<Pos> Pick;
+vector<Pos> Chicken;
+
+int Min = 9999999;
+
+//M개 치킨집 조합 구하기
+void getChicken(int x, int cnt) {
+	if (cnt == M) {
+		int res = 0;
+		for (int i = 0; i < House.size(); i++) {
+			int min_dist = 9999999;
+			//각 집에서 가장 가까운 치킨집 찾기
+			for (int j = 0; j < Pick.size(); j++) {
+				min_dist = min(min_dist, abs(House[i].x - Pick[j].x) + abs(House[i].y - Pick[j].y));
+			}
+			res += min_dist;
+		}
+		Min = min(Min, res);
+		return;
+	}
+	for (int i = x; i < Chicken.size(); i++) {
+		if (check[i] == true)
+			continue;
+		check[i] = true;
+		Pick.push_back({ Chicken[i].x, Chicken[i].y });
+		getChicken(i, cnt + 1);
+		check[i] = false;
+		Pick.pop_back();
+	}
 }
 
-int main(){
-    ios_base::sync_with_stdio(0); cin.tie(NULL);
-
-    int N, M;
-    cin >> N >> M;
-
-    vector<pair<int, int>> store; // 치킨집 위치 저장
-
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= N; j++){
-            cin >> arr[i][j];
-            if(arr[i][j] == 2) store.push_back({i, j});
-        }
-    }
-    
-    getcomb(store);
-
-
-    return 0;
+int main() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			int n;
+			cin >> n;
+			if (n == 1)
+				House.push_back({ i,j });
+			else if (n == 2)
+				Chicken.push_back({ i,j });
+		}
+	}
+	getChicken(0, 0);
+	cout << Min << '\n';
+	return 0;
 }
