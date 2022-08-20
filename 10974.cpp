10ebb4a9fd2887ada78af15e17372cbb9e3b81ef
Youngmin Park <ympark1230@naver.com>
2022-08-20 13:11:14 +0900

    백준 모든 순열 백트래킹

--- 10974.cpp
+++ 10974.cpp
@@ -21,3 +21,42 @@ int main(){
         cout << '\n';
     } while (next_permutation(v.begin(), v.end()));   
 }
+
+/*
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N;
+int visited[8+1] = {false, };
+bool flag = false;
+
+void dfs(int cnt, vector<int> v){
+    if(cnt == N){
+        for(int i = 0; i < cnt; i++){
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = 1; i <= N; i++){
+        if(!visited[i]){
+            visited[i] = true;
+            v[cnt] = i;
+            dfs(cnt+1, v);
+            visited[i] = false;
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N;
+    
+    vector<int> v(N);
+    dfs(0, v);
+}
+*/
