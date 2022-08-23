d0dcdb1a105b0bdf78b954bd9f93a0b8dbd7251a
Youngmin Park <ympark1230@naver.com>
2022-08-23 16:18:55 +0900

    백준 최단경로 다익스트라 우선순위 큐

--- 1753.cpp
+++ 1753.cpp
@@ -1,67 +1,71 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <queue>
 
-#define INF 99999999
+#define INF 1000000
+#define MAX_VERTEX 20001
+#define MAX_EDGE 300001
 
 using namespace std;
 
-vector<vector<int>> arr;
-vector<bool> visited;
-vector<int> dist;
+int d[MAX_VERTEX];
 
-int V, E, start;
+vector<pair<int, int> > edge[MAX_EDGE];
 
-int getMinIdx(){
-    int min = INF;
-    int idx = 1;
-    for(int i = 1; i <= V; i++){
-        if(dist[i] < min && !visited[i]){
-            min = dist[i];
-            idx = i;
+void dijkstra(int start_node){
+    d[start_node] = 0;
+
+    priority_queue<pair<int, int> > pq;
+    pq.push(make_pair(0, start_node));
+
+    while(!pq.empty()){
+        int current = pq.top().second;
+        int cost = -pq.top().first;
+
+        pq.pop();
+
+        if (d[current] < cost){
+            continue;
+        }
+
+        for (int i=0; i < edge[current].size(); ++i){
+            int next = edge[current][i].second;
+            int nCost = cost + edge[current][i].first;
+
+            if (d[next] > nCost){
+                d[next] = nCost;
+                pq.push(make_pair(-nCost, next));
+            }
         }
     }
-    return idx;
 }
 
 int main(){
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int v, e;
+    cin >> v >> e;
 
-    cin >> V >> E;
-    cin >> start;
+    int start_node;
+    cin >> start_node;
 
-    arr.resize(V+1, vector<int>(V+1, INF));
-    visited.resize(V+1, false);
-    dist.resize(V+1);
-
-    int a, b, c;
-    for(int i = 1; i <= E; i++){
-        cin >> a >> b >> c;
-        arr[a][b] = c;
+    for (int i=1; i<v+1; ++i){
+        d[i] = INF;
     }
 
-    for(int i = 1; i <= V; i++){
-        dist[i] = arr[start][i];
-    }
-    dist[start] = 0;
-    visited[start] = true;
+    for (int i = 0; i < e; ++i){
+        int start, end, cost;
+        cin >> start >> end >> cost;
 
-    for(int i = 1; i <= V - 1; i++){
-        int cur = getMinIdx();
-        visited[cur] = true;
-        for(int j = 1; j <= V; j++){
-            if(visited[j])
-                continue;
-            if(dist[cur] + arr[cur][j] < dist[j])
-                dist[j] = dist[cur] + arr[cur][j];
-        }
+        edge[start].push_back(make_pair(cost, end));
     }
 
-    for(int i = 1; i <= V; i++){
-        if(dist[i] == INF)
+    dijkstra(start_node);
+
+    for (int i=1; i<v+1; ++i){
+        if (d[i] == INF)
             cout << "INF\n";
-        else cout << dist[i] << '\n';
+        else
+            cout << d[i] << '\n';
     }
 
+    return 0;
 }
