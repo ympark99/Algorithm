8c790d9be00e194af92623e07b2fe53eccc29c07
Youngmin Park <ympark1230@naver.com>
2022-10-04 14:08:26 +0900

    백준 개미굴 set 문자열(우선순위큐 코드 추가)

--- 14725.cpp
+++ 14725.cpp
@@ -32,3 +32,70 @@ int main() {
 		cout << tmp << '\n';
 	}
 }
+
+/*
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+struct comp{
+    bool operator()(vector<string> a, vector<string> b){
+        int len = min(a.size(), b.size());
+        for(int i = 0; i < len; i++){
+            if(a[i] == b[i]) continue;
+            return a[i] > b[i];
+        }
+    }
+};
+
+priority_queue<vector<string>, vector<vector<string>>, comp> pq;
+int N;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        int K;
+        cin >> K;
+        vector<string> tmp;
+        for(int j = 0; j < K; j++){
+            string str;
+            cin >> str;
+            tmp.push_back(str);
+        }
+        pq.push(tmp);
+    }
+
+    vector<string> final;
+    while(!pq.empty()){
+        vector<string> str = pq.top();
+        pq.pop();
+        
+        for(int i = 0; i < str.size(); i++){
+            if(final.empty()){
+                final.push_back(str[i]);
+                cout << str[i] << '\n';
+                continue;
+            }
+            if(final.size() - 1 < i){
+                for(int j = 0; j < i; j++)
+                    cout << "--";
+                cout << str[i] << '\n';
+                continue;
+            }
+            if(final[i] == str[i]) continue;
+            else{
+                final[i] = str[i];
+                for(int j = 0; j < i; j++)
+                    cout << "--";
+                cout << str[i] << '\n';
+            }
+        }
+    }
+}
+*/
