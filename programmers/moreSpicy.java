d4f16bbf229c3ab7bb1c2ec74dceb1e1bb974a05
Youngmin Park <ympark1230@naver.com>
2022-10-30 17:04:41 +0900

    프로그래머스 더 맵게 자바

---
+++ programmers/moreSpicy.java
@@ -0,0 +1,27 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue();
+
+        for (int elem : scoville) {
+            heap.offer(elem);
+        }
+
+        while (heap.peek() <= K) {
+            if (heap.size() == 1) {
+                return -1;
+            }
+            int a = heap.poll();
+            int b = heap.poll();
+
+
+            int result = a + (b * 2);
+
+            heap.offer(result);
+            answer++;
+        }
+        return answer;
+    }
+}
