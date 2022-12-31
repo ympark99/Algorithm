c681c138ea8724b5f9f6e70b44b7ead1abd39a2f
Youngmin Park <ympark1230@naver.com>
2022-12-31 16:46:15 +0900

    프로그래머스 네트워크 dfs

---
+++ programmers/network.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void dfs(int idx, vector<vector<int>> &computers, vector<bool> &visit, int n){
+    visit[idx] = true;
+    for(int i = 0; i < n; i++){
+        if(computers[idx][i] && !visit[i])
+            dfs(i, computers, visit, n);
+    }
+    
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    
+    vector<bool> visit(n, false);
+    for(int i = 0; i < n; i++){
+        if(!visit[i]){
+            answer++;
+            dfs(i, computers, visit, n);
+        }
+    }
+    
+    return answer;
+}
