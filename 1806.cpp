bc859e69265a3a09cb018b68caef6487d3796124
Youngmin Park <ympark1230@naver.com>
2023-01-09 14:16:20 +0900

    백준 부분합 투포인터

--- 1806.cpp
+++ 1806.cpp
@@ -1,35 +1,31 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int arr[100001];
+int arr[100000+1];
 
-int main(){
+int main(void) {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     int N, S;
     cin >> N >> S;
-
-    for(int i = 1; i <= N; i++) 
+    for(int i = 1; i <= N; i++)
         cin >> arr[i];
 
-    int start = 1;
-    int end = 1;
+    int start = 1, end = 1;
     int sum = arr[1];
-    int ans = 987654321;
-
+    int ans = INT_MAX;
     while(start <= end && end <= N){
-        if(sum >= S) ans = min(ans, (end - start + 1));
-        if(sum < S) {
-            end++; 
-            sum += arr[end];
-        } else {
+        if(sum >= S) ans = min(ans, end-start+1);
+        if(sum < S){
+            end++;
+            sum += arr[end]; 
+        }else{
             sum -= arr[start];
             start++;
         }
     }
-
-    if(ans == 987654321) cout << 0;
-    else cout << ans;
+    if(ans == INT_MAX) cout << 0 << '\n';
+    else cout << ans << '\n';
+	return 0;
 }
