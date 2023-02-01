eff22e9a725fac5f9b15b2e7ce4e8c9b0cbdc05f
Youngmin Park <ympark1230@naver.com>
2023-02-01 14:51:37 +0900

    백준 퇴사 dp

--- 14501.cpp
+++ 14501.cpp
@@ -1,46 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int answer = 0;
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-int comp(int sum){
-    answer = max(answer, sum);
-}
+    int N; cin >> N;
+    vector<int> dp(N+2, 0);
+    vector<pair<int, int>> v(N+2);
 
-void dfs(int N, int nowtime, int sum, vector<pair<int, int>> v){
+    for(int i = 1; i <= N; i++)
+        cin >> v[i].first >> v[i].second;
 
-    int nextTime = nowtime + v[nowtime - 1].first;
-    int nextSum = sum + v[nowtime - 1].second;
-
-    if(nextTime - 1 > N) return;
-
-    comp(nextSum);
-    dfs(N, nextTime, nextSum, v);
-    //cout << "next time : " << nextTime << "\n";
-    return;
-}
-
-int main(){
-    int N;
-    cin >> N;
-
-    vector<pair<int, int>> v;
-
-    for(int i = 0 ; i < N; i++){
-        int timeLength, price;
-        cin >> timeLength >> price;
-        v.push_back(make_pair(timeLength, price));
+    for(int i = N; i > 0; i--){
+        int cost = v[i].first;
+        int reward = v[i].second;
+        if(i + cost > N+1){
+            dp[i] = dp[i+1];
+        }
+        else
+            dp[i] = max(dp[i+1], dp[i + cost] + reward);
+        
     }
-
-    int starttime = 1;
-    int sum = 0;
-    while(starttime <= N){
-        dfs(N, starttime, sum, v);
-        starttime++;
-    //    cout << "start time : " << starttime << "\n";
-    } 
-
-    cout << answer;
+    cout << dp[1] << '\n';
 }
