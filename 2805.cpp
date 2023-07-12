ca736ead6159ca6832d052f74b0350583f9f525f
Youngmin Park <ympark1230@naver.com>
2023-07-12 16:08:48 +0900

    백준 나무 자르기 이분탐색

--- 2805.cpp
+++ 2805.cpp
@@ -1,45 +1,45 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int arr[1000000];
 int N, M;
 
-int bSearch(int end)
-{
-        int start = 0;
-        int height = 0;
-        long long sum = 0;
-
-        while (start <= end) {
-                sum = 0;
-                height = (start + end) / 2;
-
-                for (int i = 0; i < N; i++) {
-                        if (arr[i] > height)
-                                sum += (arr[i] - height);
-                }
-
-                if (sum >= M) {
-                        start = height + 1;
-                }
-                else {
-                        end = height - 1;
-                }
-        }
-
-        return end;
+bool cal(vector<int> v, int mid) {
+	long long sum = 0;
+	for(auto elem: v) {
+		if(elem > mid)
+			sum += (elem - mid);
+	}
+	if(sum >= M) 
+		return true;
+	else 
+		return false;
 }
 
-int main(void) 
-{
-        scanf("%d %d", &N, &M);
-        for (int i = 0; i < N; i++) 
-                scanf("%d", &arr[i]);
+int main(void) {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-        sort(arr, arr + N);
-        printf("%d", bSearch(arr[N-1]));
+	cin >> N >> M;
 
-        return 0;
+	vector<int> v(N);
+
+	for(int i = 0; i < N; i++)
+		cin >> v[i];
+
+	long long left = 0;
+	long long right = *max_element(v.begin(), v.end());
+	long long ans = 0;
+
+	while(left <= right) {
+		long long mid = (left + right) / 2;
+		if(cal(v, mid)) {
+			ans = max(ans, mid);
+			left = mid + 1;
+		}
+		else right = mid - 1;
+	}
+
+	cout << ans << '\n';
+
+	return 0;
 }
