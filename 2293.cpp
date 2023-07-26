f440c9b6d85d45133d6a2ac6fff8bb8b56e3e495
Youngmin Park <ympark1230@naver.com>
2023-07-26 13:46:43 +0900

    백준 동전 1 dp

--- 2293.cpp
+++ 2293.cpp
@@ -1,26 +1,56 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int dp[10000+1];
+int n, k;
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+int dp[10001];
 
-    int n, k; cin >> n >> k;
-    vector<int> v(n);
-    for(int i = 0; i < n; i++)
-        cin >> v[i];
+int main(void) {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-    dp[0] = 1;
+	cin >> n >> k;
+	vector<int> coin(n);
+	for(int i = 0; i < n; i++) {
+		cin >> coin[i];
+	}
 
-    for(int i = 0; i < v.size(); i++){
-        for(int j = v[i]; j <= k; j++){
-            dp[j] += dp[j - v[i]];
-        }
-    }
-    cout << dp[k] << '\n';    
-    return 0;
+	dp[0] = 1;
+
+	for(int i = 0; i < coin.size(); i++) {
+		for(int j = coin[i]; j <= k; j++)
+			dp[j] += dp[j - coin[i]];
+	}
+
+	cout << dp[k] << '\n';
+
+	return 0;
 }
+
+
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// int dp[10000+1];
+
+// int main() {
+//     ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+//     int n, k; cin >> n >> k;
+//     vector<int> v(n);
+//     for(int i = 0; i < n; i++)
+//         cin >> v[i];
+
+//     dp[0] = 1;
+
+//     for(int i = 0; i < v.size(); i++){
+//         for(int j = v[i]; j <= k; j++){
+//             dp[j] += dp[j - v[i]];
+//         }
+//     }
+//     cout << dp[k] << '\n';    
+//     return 0;
+// }
