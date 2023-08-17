17d30dbe759d8822dba99815104f946e45ba06ff
Youngmin Park <ympark1230@naver.com>
2023-08-17 18:07:17 +0900

    백준 점프 dp

--- 1890.cpp
+++ 1890.cpp
@@ -1,59 +1,38 @@
-#include<iostream>
+#include <bits/stdc++.h>
  
-typedef long long ll;
-#define endl "\n"
-#define MAX 100
 using namespace std;
- 
+
 int N;
-int MAP[MAX][MAX];
-ll DP[MAX][MAX];
- 
-void Input()
-{
-    cin >> N;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            cin >> MAP[i][j];
-        }
-    }
-    DP[0][0] = 1;
-}
- 
-void Solution()
-{
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            if (DP[i][j] == 0 || (i == N - 1 && j == N - 1)) continue;
-            
-            int Value = MAP[i][j];
-            int Down = Value + i;
-            int Right = Value + j;
- 
-            if (Down < N) DP[Down][j] = DP[Down][j] + DP[i][j];
-            if (Right < N) DP[i][Right] = DP[i][Right] + DP[i][j];
-        }
-    }
-    cout << DP[N - 1][N - 1] << endl;
-}
- 
-void Solve()
-{
-    Input();
-    Solution();
-}
- 
-int main(void)
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
- 
-    Solve();
- 
+int arr[101][101];
+long long dp[101][101];
+
+int main(void) {
+    ios::sync_with_stdio(NULL); cin.tie(0); cout.tie(0);
+
+	cin >> N;
+
+	for(int i = 1; i <= N; i++) {
+		for(int j = 1; j <= N; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	dp[1][1] = 1;
+
+	for(int i = 1; i <= N; i++) {
+		for(int j = 1; j <= N; j++) {
+			if(!dp[i][j] || (i == N && j == N)) continue;
+
+			long long down = i + arr[i][j];
+			long long right = j + arr[i][j];
+
+			if(down <= N)
+				dp[down][j] += dp[i][j];
+			if(right <= N)
+				dp[i][right] += dp[i][j];
+		}
+	}
+	
+	cout << dp[N][N] << '\n';
+
     return 0;
 }
