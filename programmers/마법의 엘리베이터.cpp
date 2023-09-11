297ab2f5172293cfe00e044ce7e3dac427452372
Youngmin Park <ympark1230@naver.com>
2023-09-11 13:14:06 +0900

    프로그래머스 마법의 엘리베이터 그리디

---
+++ programmers/마법의 엘리베이터.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int storey) {
+    int answer = 0;
+
+    while (storey)
+    {
+        int cur = storey % 10;
+        int next = (storey / 10) % 10;
+
+        if (cur > 5)
+        {
+            answer += 10 - cur;
+            storey += 10;
+        }
+        else if (cur == 5)
+        {
+            answer += 5;
+            storey += next >= 5 ? 10 : 0;
+        }
+        else
+        {
+            answer += cur;
+        }
+
+        storey /= 10;
+    }
+
+    return answer;
+}
