26693a8567c5dd94b445b5ed0fec9f17dd7f503d
Youngmin Park <ympark1230@naver.com>
2023-09-11 12:45:47 +0900

    프로그래머스 택배상자 스택

---
+++ programmers/택배상자.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> order) {
+    int answer = 0;
+    
+    stack<int> st;
+    for(int i = 1; i <= order.size(); i++) {
+        st.push(i);
+        while(!st.empty() && st.top() == order[answer]) {
+            st.pop();
+            answer++;
+        }
+    }
+    
+    return answer;
+}
