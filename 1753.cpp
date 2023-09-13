adf4cae4a15cd4c47b4ff99a1df4edf00940ab84
Youngmin Park <ympark1230@naver.com>
2023-09-13 14:39:15 +0900

    백준 최단경로 다익스트라

--- 1753.cpp
+++ 1753.cpp
@@ -1,71 +1,54 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
-#define INF 1000000
-#define MAX_VERTEX 20001
-#define MAX_EDGE 300001
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int d[MAX_VERTEX];
+#define INF 987654321
 
-vector<pair<int, int> > edge[MAX_EDGE];
+int V, E, K;
+int dist[20001];
+vector<pair<int, int>> edge[20001];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-void dijkstra(int start_node){
-    d[start_node] = 0;
+void dijkstra() {
+	pq.push({0, K});
+	dist[K] = 0;
 
-    priority_queue<pair<int, int> > pq;
-    pq.push(make_pair(0, start_node));
+	while(!pq.empty()) {
+		int cost = pq.top().first;
+		int cur = pq.top().second;
+		pq.pop();
 
-    while(!pq.empty()){
-        int current = pq.top().second;
-        int cost = -pq.top().first;
+		for(int i = 0; i < edge[cur].size(); i++) {
+			int next = edge[cur][i].first;
+			int ncost = edge[cur][i].second;
 
-        pq.pop();
-
-        if (d[current] < cost){
-            continue;
-        }
-
-        for (int i=0; i < edge[current].size(); ++i){
-            int next = edge[current][i].second;
-            int nCost = cost + edge[current][i].first;
-
-            if (d[next] > nCost){
-                d[next] = nCost;
-                pq.push(make_pair(-nCost, next));
-            }
-        }
-    }
+			if(dist[next] > cost + ncost) {
+				dist[next] = cost + ncost;
+				pq.push({dist[next], next});
+			}
+		}
+	}
 }
 
-int main(){
-    int v, e;
-    cin >> v >> e;
+int main(void){
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-    int start_node;
-    cin >> start_node;
+	cin >> V >> E >> K;
 
-    for (int i=1; i<v+1; ++i){
-        d[i] = INF;
-    }
+	for(int i = 0; i < 20001; i++)
+		dist[i] = INF;
 
-    for (int i = 0; i < e; ++i){
-        int start, end, cost;
-        cin >> start >> end >> cost;
+	for(int i = 0; i < E; i++) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		edge[u].push_back({v, w});
+	}
 
-        edge[start].push_back(make_pair(cost, end));
-    }
+	dijkstra();
 
-    dijkstra(start_node);
-
-    for (int i=1; i<v+1; ++i){
-        if (d[i] == INF)
-            cout << "INF\n";
-        else
-            cout << d[i] << '\n';
-    }
-
-    return 0;
+	for(int i = 1; i <= V; i++) {
+		if(dist[i] == INF)
+			cout << "INF\n";
+		else cout << dist[i] << '\n';
+	}
 }
