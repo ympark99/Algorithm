e67a9506474ca66782143281993ead01d8204423
Youngmin Park <ympark1230@naver.com>
2023-10-16 16:41:51 +0900

    프로그래머스 시소 짝꿍 수학

---
+++ programmers/시소 짝꿍.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(vector<int> weights) {
+    long long answer = 0;
+    vector<long long> cnt(1001, 0);
+    
+    for(int i = 0; i < weights.size(); i++)
+        cnt[weights[i]]++;
+    
+    for(int i = 0; i < weights.size(); i++) {
+        //2:3
+        if(weights[i] % 2 == 0) {
+            long long base = (weights[i] / 2) * 3;
+            if(base <= 1000)    
+                answer += cnt[base];
+        }
+        //3:4
+        if(weights[i] % 3 == 0) {
+            long long base = (weights[i] / 3) * 4;
+            if(base <= 1000)    
+                answer += cnt[base];
+        }
+        //1:2
+        long long base = weights[i] * 2;
+        if(base <= 1000)    
+            answer += cnt[base];
+    }
+    
+    for(int i = 100; i <= 1000; i++) {
+        if(cnt[i] >= 2)
+            answer += (long long)(cnt[i] * (cnt[i]-1)) / 2;
+    }
+    
+    return answer;
+}
