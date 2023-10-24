dff4a16f607880115da7334c18f44f7ee4e35b0e
Youngmin Park <ympark1230@naver.com>
2023-10-24 21:06:13 +0900

    백준 보물 정렬

--- 1026.cpp
+++ 1026.cpp
@@ -1,32 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(){
-    int N;
-    cin >> N;
-    vector<int> v1;
-    vector<int> v2;
-    int answer = 0;
+int N;
 
-    for(int i = 0; i < N; i++){
-        int num;
-        cin >> num;
-        v1.push_back(num);
-    }
-    for(int i = 0; i < N; i++){
-        int num;
-        cin >> num;
-        v2.push_back(num);
-    }    
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end(), greater<int>());
+int main(void){
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-    for(int i = 0; i < N; i++){
-        answer += v1[i] * v2[i];
-    }
+	vector<int> a;
+	vector<int> b;
+	int ans = 0;
 
-    cout << answer;
+	cin >> N;
+
+	int num;
+	for(int i = 0; i < N; i++) {
+		cin >> num;
+		a.push_back(num);
+	}
+
+	for(int i = 0; i < N; i++) {
+		cin >> num;
+		b.push_back(num);
+	}
+
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end(), greater<>());
+
+	for(int i = 0; i < N; i++) {
+		ans += a[i] * b[i];
+	}
+
+	cout << ans << '\n';
+
+	return 0;
 }
